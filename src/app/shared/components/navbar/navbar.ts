98489025ab728b3bc810c35748ac630a794c8352
Alexander Esteban <alexanderesteban7@gmail.com>
2026-09-04 15:11:26 -0500

    feat: implement responsive Navbar component with dynamic routing and page title synchronization

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, AsyncPipe],
   templateUrl: './navbar.html',
   changeDetection: ChangeDetectionStrategy.Eager,
 })
-export class Navbar {}
+export class Navbar {
+
+  private router = inject(Router);
+  routes = routes
+    .filter(route => route.path != '**')
+    .map(route => ({
+      path: route.path,
+      title: `${route.title ?? 'Maps in Angular'}`
+    }));
+
+  pageTitle$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    map(event => event.url),
+    map(url => routes.find(route => `/${route.path}` === url)?.title)
+  )
+}
