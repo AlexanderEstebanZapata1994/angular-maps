d87dfa954fd8f104c8732627112c973acc979cec
Alexander Esteban <alexanderesteban7@gmail.com>
2026-09-04 14:47:30 -0500

    feat: initialize angular project with routing, page scaffolding, and tailwind/daisyui configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
+import { HousesPage } from './pages/houses-page/houses-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    title: 'Fullscreen Map',
+    component: FullscreenMapPage
+  },
+  {
+    path: 'markers',
+    title: 'Markers Map',
+    component: MarkersPage
+  },
+  {
+    path: 'houses',
+    title: 'Houses Map',
+    component: HousesPage
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen'
+  },
+];
