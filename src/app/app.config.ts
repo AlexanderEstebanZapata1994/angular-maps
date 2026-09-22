991a3206eedd8b0c5c75012d623a2446c8bc11f4
Alexander Esteban <alexanderesteban7@gmail.com>
2026-09-22 18:11:56 -0500

    fix: Add hash strategy to app.config.ts file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+
+    // Hash Strategy
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+
+    }
   ]
 };
